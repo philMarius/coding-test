0451f5f8ad04df2657a31f193d1bdbafcb5bd74a
Philip Cumner <philipcumner@gmail.com>
2019-09-08 13:01:01 +0100

    Add comments to REST API

--- q2.py
+++ q2.py
@@ -1,4 +1,8 @@
-'''Simple REST API.'''
+'''Simple REST API.
+
+There is a lot that can be done to improve the code here but I wanted to submit
+a minimum viable product.
+'''
 
 from flask import Flask, request, make_response, jsonify
 import sqlite3 as sql
@@ -26,9 +30,12 @@ def index():
 
 @app.route('/recipe/create', methods=['POST'])
 def create_recipe():
-    result = {'success': 'false'}
+    '''Create recipe as POST request.'''
+    # Default response is server error
+    result = make_response(jsonify({'success': 'false'}), 500)
 
     if request.method == 'POST':
+        # Pull out data from request
         name = request.form['name']
         description = request.form['description']
         ingredients = request.form['ingredients']
@@ -43,13 +50,14 @@ def create_recipe():
             ''', (name, description, ingredients))
             conn.commit()
 
-            result['success'] = 'true'
+            result = make_response(jsonify({'success': 'true'}), 200)
 
     return result
 
 @app.route('/recipe/edit', methods=['PATCH',])
 def edit_recipe():
     '''Update recipe in table.'''
+    # Default response is server error
     result = make_response(jsonify({'success': 'false'}), 500)
 
     data = request.form
@@ -85,16 +93,20 @@ def edit_recipe():
         conn.commit()
 
         if update == 1:
-            result = make_response(jsonify({'success': 'true'}))
+            result = make_response(jsonify({'success': 'true'}), 200)
         elif update == 0:
-            result = make_response(jsonify({'error': 'ID not found'}))
+            result = make_response(jsonify({'error': 'ID not found'}), 404)
 
     return result
 
 @app.route('/recipe', methods=['GET',])
 def get_recipe():
-    '''Get recipe from table.'''
-    result = None
+    '''Get recipe from table.
+
+    If no `recipe_id` submitted, then all recipes are returned
+    '''
+    # Default response is server error
+    result = make_response(jsonify({'success': 'false'}), 500)
 
     recipe_id = request.args.get('recipe_id', default=None)
 
@@ -119,12 +131,14 @@ def get_recipe():
 
         if result is None:
             # Return error if no recipe found for ids
-            return {'error': 'Recipe not found'}
+            return make_response(jsonify({'error': 'Recipe not found'}), 404)
 
     return result
 
 @app.route('/recipe/delete', methods=['DELETE',])
 def delete_recipe():
+    '''Delete recipe using `recipe_id`.'''
+    # Default response is server error
     result = make_response(jsonify({'success': 'false'}), 500)
 
     recipe_id = request.args.get('recipe_id', default=None)
@@ -145,17 +159,15 @@ def delete_recipe():
         # Check if deletion was successful, rowcount = 1 for success
         if delete == 1:
             # Successful deletion
-            result['success'] = 'true'
             result = make_response(jsonify({'success': 'true'}), 200)
         elif delete == 0:
             # Unsuccessful deletion
-            result['error'] = 'ID not found'
             result = make_response(jsonify({'error': 'ID not found'}), 404)
 
     return result
 
 def create_db():
-    '''Create database when starting flask.'''
+    '''Create database when starting flask and add dummy data.'''
     with sql.connect(db_name) as conn:
         conn.row_factory = dict_factory
         c = conn.cursor()
